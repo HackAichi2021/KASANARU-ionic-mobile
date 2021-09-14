37b787bd9ff2f7cf8527405b34bee548bb3424cb
Sakyuba Noe <398noe@gmail.com>
2021-09-14 10:20:30 +0900

    Basic setup is completed

---
+++ kasanaru-mobile/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [
+    "./src/**/*.{html,js}"
+  ],
+  darkMode: "class", // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+}
