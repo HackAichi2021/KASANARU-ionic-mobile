fdf21d4298eb3d1b326d94f0c79979ba0a6d8e88
SakabeTomoya <sakabe@monoxer.com>
2021-09-15 10:56:10 +0900

    not complete post

--- kasanaru-mobile/src/app/login/login.page.ts
+++ kasanaru-mobile/src/app/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient }    from '@angular/common/http';
+import { User } from '../models/user';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+
+  private basedUrl = 'https://hackaichi2021.herokuapp.com/';
+  private user = {
+    email: 'neko',
+    password: 'dda',
+    username: 'dsa',
+    age: 43
+  };
+
+  constructor(private httpclient: HttpClient){
+  }
+
+  getData(): void{
+    this.httpclient.get<User>(this.basedUrl).subscribe(res => {
+      console.log(res);
+    });
+  }
+
+  authData(): void{
+    this.httpclient.post<User>(`${this.basedUrl}/api/user/register}`, this.user).subscribe(res => {
+      console.log(res);
+    });
+  }
 
   ngOnInit() {
   }
