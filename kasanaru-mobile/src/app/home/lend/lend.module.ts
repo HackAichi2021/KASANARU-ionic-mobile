3ba2ef71221c32eed3411d1fc10761b0a85e686a
Kosuke Suzuki <k.suzuki764@gmail.com>
2021-09-16 02:37:43 +0900

    lendにdestination-mapを適用

--- kasanaru-mobile/src/app/home/lend/lend.module.ts
+++ kasanaru-mobile/src/app/home/lend/lend.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { GoogleMapsModule } from '@angular/google-maps';
+import { DestinationMapComponent } from 'src/app/components/destination-map/destination-map.component';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -13,8 +15,9 @@ import { LendPage } from './lend.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    LendPageRoutingModule
+    LendPageRoutingModule,
+    GoogleMapsModule
   ],
-  declarations: [LendPage]
+  declarations: [LendPage, DestinationMapComponent]
 })
-export class LendPageModule {}
+export class LendPageModule { }
