f450f5ad441d9636513605765e08776135c6ab69
Kosuke Suzuki <k.suzuki764@gmail.com>
2021-09-16 20:20:27 +0900

    サーバーからデータを取得して表示

---
+++ kasanaru-mobile/src/app/models/favorite.ts
@@ -0,0 +1,32 @@
+export class Favorite {
+  sex: number
+  game: number
+  sport: number
+  book: number
+  travel: number
+  internet: number
+  anime: number
+  movie: number
+  music: number
+  gourmet: number
+  muscle: number
+  camp: number
+  tv: number
+  cook: number
+  constructor(sex: number, game: number, sport: number, book: number, travel: number, internet: number, anime: number, movie: number, music: number, gourmet: number, mucle: number, camp: number, tv: number, cook: number) {
+    this.sex = sex
+    this.game = game
+    this.sport = sport
+    this.book = book
+    this.travel = travel
+    this.internet = internet
+    this.anime = anime
+    this.movie = movie
+    this.music = music
+    this.gourmet = gourmet
+    this.muscle = mucle
+    this.camp = camp
+    this.tv = tv
+    this.cook = cook
+  }
+}
