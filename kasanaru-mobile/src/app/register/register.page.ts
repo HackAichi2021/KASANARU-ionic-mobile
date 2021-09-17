6b6630e41166c39017f23fb400136e3db8572fad
Sakyuba Noe <398noe@gmail.com>
2021-09-18 04:38:29 +0900

    Wanna Delete Register

--- kasanaru-mobile/src/app/register/register.page.ts
+++ kasanaru-mobile/src/app/register/register.page.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit, NgModule } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from "@angular/forms";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Validators } from "@angular/forms";
+import { User } from "../models/user";
+import { Store } from "../store/store";
 
 @Component({
     selector: 'app-register',
@@ -9,20 +12,45 @@ import { Validators } from "@angular/forms";
 })
 
 export class RegisterPage implements OnInit {
-    registerForm = this.builder.group({
-        email: ["", Validators.required],
-        password: ["", Validators.required],
-        username: ["", Validators.required],
-        age: ["", Validators.required],    
-    });
+    private basedUrl = 'https://hackaichi2021.herokuapp.com/';
+    registerForm: FormGroup;
+    model: User;
+    private message = "Text";
 
-    constructor(private builder: FormBuilder) {}
+    // Input to User Model
+    
+    constructor(private builder: FormBuilder, private http: HttpClient, private store: Store) {
+        this.registerForm = this.builder.group({
+            email: ["", Validators.required],
+            password: ["", Validators.required],
+            username: ["", Validators.required],
+            age: ["", Validators.required],    
+        });
+        this.message = this.store.getMessage();
+    }
 
     /**
      * POST to Server
      */
-    register() {
-        console.log(this.registerForm.value);
+    register(): void {
+        const httpOptions = {
+            headers: new HttpHeaders({
+                'Content-Type': 'application/json'
+            }),
+            observe: "response"
+        }
+        this.http.post<any>(`${this.basedUrl}api/user/register`,
+            JSON.stringify(this.registerForm.value)).subscribe(
+                (res) => {
+                    if(res.status == "Success") {
+                        console.log("Success!");
+                        localStorage.setItem("access_token", res.access_token);
+                        localStorage.setItem("refresh_token", res.refresh_token);
+                    } else {
+                        console.log("Failed");
+                    }
+                }
+            );
     }
     ngOnInit(): void {
     }
