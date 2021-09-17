4ef4420b8f4d8445a46588e250588252e21dcdbf
Kosuke Suzuki <k.suzuki764@gmail.com>
2021-09-17 13:07:29 +0900

    マッチング情報のpost

--- kasanaru-mobile/src/app/home/borrow/check/check.page.ts
+++ kasanaru-mobile/src/app/home/borrow/check/check.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Store } from '../../../store/store';
+import { LendAndBorrow } from 'src/app/models/lendAndborrow';
+import { Token } from 'src/app/models/token';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-check',
@@ -9,7 +12,12 @@ import { Store } from '../../../store/store';
 })
 export class CheckPage implements OnInit {
 
-  constructor(private httpclient: HttpClient, private store: Store) { }
+  private basedUrl = 'https://hackaichi2021.herokuapp.com/';
+
+  // あとで置き換える
+  testToken = new Token("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3NfdXVpZCI6IjFlMGRiNTgxLTJiMWYtNDg5Ny1iMzkxLWM2NDgyYTQ4NzU1NCIsImF1dGhvcml6ZWQiOnRydWUsImV4cCI6MTYzMTc4MzY0MCwidXNlcl9pZCI6Mn0.xm5Wdro-DjcdGHUlFRm-L17hodbpsa-hTIrv6HeSh9E", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MzIzODc1NDAsInJlZnJlc2hfdXVpZCI6IjYxNThhMjZkLTU2MjgtNGIzMi05YmQzLTI5OGQwYjNiYTc5NSIsInVzZXJfaWQiOjJ9.Ds69R712NC4dhXYKqaK5xB-ZUWEozQPtm0ScFccJAmY")
+
+  constructor(private httpclient: HttpClient, private store: Store, private router: Router) { }
 
   ngOnInit() {
   }
@@ -17,6 +25,18 @@ export class CheckPage implements OnInit {
   postLendAndBorrow(x: number): void {
     this.store.setAfterOK(x);
     // ここで3つの情報が揃ったのでpostする
+    console.log(this.store.getLendAndBorrow())
+
+    var lendAndBorrow: LendAndBorrow = this.store.getLendAndBorrow()
+    var body = { ...lendAndBorrow, ...this.testToken }
+    var headers = new HttpHeaders()
+
+    this.httpclient.post(`${this.basedUrl}api/user/matching`, body, {
+      headers: headers.set('Access-Control-Allow-Origin', '*'),
+    }).subscribe(res => {
+      console.log(res);
+      this.router.navigateByUrl('borrow_matched');
+    });
   }
 
 }
--- kasanaru-mobile/src/app/home/lend/check/check.page.ts
+++ kasanaru-mobile/src/app/home/lend/check/check.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Store } from '../../../store/store';
+import { LendAndBorrow } from 'src/app/models/lendAndborrow';
+import { Token } from 'src/app/models/token';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-check',
@@ -9,7 +12,12 @@ import { Store } from '../../../store/store';
 })
 export class CheckPage implements OnInit {
 
-  constructor(private httpclient: HttpClient, private store: Store) { }
+  private basedUrl = 'https://hackaichi2021.herokuapp.com/';
+
+  // あとで置き換える
+  testToken = new Token("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3NfdXVpZCI6IjE2NDM3ZjE1LTBkMDAtNDZjOS1iZjI5LTYzMmU4MzhiMDEwOSIsImF1dGhvcml6ZWQiOnRydWUsImV4cCI6MTYzMTg0MzU0MSwidXNlcl9pZCI6M30.dzxYufEwr48yvUmR1gYAY3C6YsHowY9E-asCaWz8G-w", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MzI0NDc0NDEsInJlZnJlc2hfdXVpZCI6ImEzYTFhNjQ5LTdiOWUtNDVkOC05YWQ1LWNmYTRlYjhmOTZkYyIsInVzZXJfaWQiOjN9.wz02B9Ze2Q9mFP9vwmcwtDfsjQIpOdiU_u5zW4Pg5xA")
+
+  constructor(private httpclient: HttpClient, private store: Store, private router: Router) { }
 
   ngOnInit() {
   }
@@ -17,6 +25,18 @@ export class CheckPage implements OnInit {
   postLendAndBorrow(x: number): void {
     this.store.setAfterOK(x);
     // ここで3つの情報が揃ったのでpostする
+    console.log(this.store.getLendAndBorrow())
+
+    var lendAndBorrow: LendAndBorrow = this.store.getLendAndBorrow()
+    var body = { ...lendAndBorrow, ...this.testToken }
+    var headers = new HttpHeaders()
+
+    this.httpclient.post(`${this.basedUrl}api/user/matching`, body, {
+      headers: headers.set('Access-Control-Allow-Origin', '*'),
+    }).subscribe(res => {
+      console.log(res);
+      this.router.navigateByUrl('lend_matched');
+    });
   }
 
 }
